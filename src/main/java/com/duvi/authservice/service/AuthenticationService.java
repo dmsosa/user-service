ab9197b39975f95ef8a545a75ecfcab23d1cfff4
Durian Sosa <dmsosa21@outlook.com>
2023-12-15 19:59:39 +0000

    Readme.MD hinzugefugt

---
+++ src/main/java/com/duvi/authservice/service/AuthenticationService.java
@@ -0,0 +1,21 @@
+package com.duvi.authservice.service;
+
+import com.duvi.authservice.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username);
+    }
+}

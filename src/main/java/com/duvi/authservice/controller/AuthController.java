ff2b1f6e4dc2bb2b7e0887a8cd1fab24effc2d77
Durian Sosa <dmsosa21@outlook.com>
2023-12-15 20:16:10 +0000

    CORS hinzugefugt

--- src/main/java/com/duvi/authservice/controller/AuthController.java
+++ src/main/java/com/duvi/authservice/controller/AuthController.java
@@ -17,6 +17,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = {"*"})
 @Slf4j
 @RestController
 @RequestMapping("/api/auth")
